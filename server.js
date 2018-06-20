509d793104b7ae08e0fdb1ce0f69d213b3926eb3
Animesh <animeshmane@gmail.com>
2018-06-20 17:48:00 -0400

    heroku deployment1

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname+'/dist/assignment5'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname + '/dist/assignment5/src/index.html'));
+res.sendFile(path.join(__dirname,'/dist/assignment5/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
